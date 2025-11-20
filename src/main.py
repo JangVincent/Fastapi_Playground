fdcd0bb6d1a7050bcd56811c6fbf1cd1dc1b6cca
JangVincent <vincentjangdev@gmail.com>
2025-11-20 09:37:10 +0900

    add : logger

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+import logging
+
 from fastapi import FastAPI
 
 from src.config import settings
+from src.core.logger import init_logger
 from src.domains.user.router import router as user_router
+
+init_logger()
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     redoc_url=None if settings.environment == "prod" else "/redoc",
